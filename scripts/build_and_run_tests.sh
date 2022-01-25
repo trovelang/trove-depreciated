9608a4e27fa3d091f0b85e49816481badc682df1
James Clarke <jkc200013@gmail.com>
2022-01-25 19:56:43 +0000

    fixed ci/cd

--- scripts/build_and_run_tests.sh
+++ scripts/build_and_run_tests.sh
@@ -1,3 +1,3 @@
-cmake --build c:/trovelang/trove/build --config Debug --target ALL_BUILD -j 14 --
+cmake --build build --config Debug --target ALL_BUILD -j 14 --
 cd build
 ctest -j14 -C Debug -T test --output-on-failure
